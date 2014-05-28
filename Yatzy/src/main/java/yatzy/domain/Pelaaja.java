6d4b1dea66f6a58e8fd70e3b259865d3481fb279
Minna Hannula <minna.k.hannula@helsinki.fi>
2014-05-28 09:34:59 +0300

    muutama paivitys

--- Yatzy/src/main/java/yatzy/domain/Pelaaja.java
+++ Yatzy/src/main/java/yatzy/domain/Pelaaja.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 public class Pelaaja {
     
     private String nimi;
+    public int pisteet;
     
     
-    public Pelaaja(String nimi){
-        
+    public Pelaaja(String nimi){ 
         this.nimi = nimi;
+        this.pisteet = 0;
 
     }
     
